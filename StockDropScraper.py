3fd234c4c4290681f6599c1c00b1b41a3e4a5d8a
mike-le <michael.le@richmond.edu>
2019-03-17 21:07:22 -0400

    Add execution time log

--- StockDropScraper.py
+++ StockDropScraper.py
@@ -7,6 +7,7 @@ from urllib import request
 import PyPDF2
 import os
 import io
+import time
 
 _DIRECTORY = "pdfs/"
 
@@ -35,7 +36,6 @@ class PDF:
             pageObj = pdfReader.getPage(0)
             text = str(pageObj.extractText()).split('ORTRANSITION')[0]
             numbers = [int(text) for text in text.split() if text.isdigit()]
-            print(text)
             if len(numbers) > 0:
                 return numbers[len(numbers)-1]
             else:
@@ -70,11 +70,11 @@ def get_report():
     """
     urls = []
     names = []
-    #_DOMAIN = "https://ir.aboutamazon.com/sec-filings?field_nir_sec_form_group_target_id%5B%5D=471&field_nir_sec_date_filed_value=&items_per_page=10"
     _TICKER = "GOOGL"
     _DOMAIN = "https://abc.xyz/"
     _URL = _DOMAIN + "investor"
     response = simple_get(_URL)
+    start = time.time()
 
     if response is not None:
         response = BeautifulSoup(response, 'html.parser')
@@ -109,6 +109,8 @@ def get_report():
                             numberOfFiles += 1
             except RequestException as e:
                 log_error('Error during requests to {0} : {1}'.format(_URL, str(e)))
+        end = time.time()
+        print('Execution Time: ' + str(round(end - start, 2)) + ' seconds')
         print('Number of files copied over: ' + str(numberOfFiles))
     else:
         # Raise an exception if we failed to get any data from the url

6c5c3a29d1f9077e2662d13cda1d4602453f894e
mike-le <michael.le@richmond.edu>
2019-03-22 13:45:57 -0400

    Add unit tests

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -11,6 +11,15 @@ class TestScraper(unittest.TestCase):
         result = True if StockDropScraper.simple_get(url) is not None else False
         self.assertTrue(result)
 
+    def test_is_10K(self):
+        """
+        Test PDF object to see if it is a 10-K report
+        """
+        pdf = '/test-resources/test.pdf'
+        pdf_bytes = open(pdf, 'rb')
+        pdfObj = StockDropScraper.PDF(pdf_bytes, 'test.pdf')
+        self.assertTrue(pdfObj.is_10K())
+
 
 if __name__ == '__main__':
     unittest.main()
